409fadf5ceeffd053073552cae6ec65c4c1802c4
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-06 08:47:58 -0600

    Initial Commit

---
+++ SaveMoney/SaveMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveMoneyApp.swift
+//  SaveMoney
+//
+//  Created by J. DeWeese on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SaveMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
